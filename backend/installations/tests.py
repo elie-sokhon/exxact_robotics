f977b34ee1418d7f197d6e12f32ee4cf18fbb276
elie-sokhon <elieelsokhon@gmail.com>
2025-07-10 12:07:20 +0200

    d

--- backend/installations/tests.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from django.urls import reverse
-from installations.models import Installation
-
-@pytest.mark.django_db
-def test_installation_list(client):
-    url = reverse("installation-list")
-    response = client.get(url)
-    assert response.status_code == 200
